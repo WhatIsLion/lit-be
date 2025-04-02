2e291c40ef937f7b857534e91924564a96f9bcf8
yunshin <50646145+transparentdeveloper@users.noreply.github.com>
2025-04-02 18:30:12 +0900

    FEAT:(CORE) 스프링부트 프로젝트 초기 설정

---
+++ src/main/java/kr/teamo2/lit/LitApplication.java
@@ -0,0 +1,13 @@
+package kr.teamo2.lit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/teamo2/lit/LitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.teamo2.lit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
